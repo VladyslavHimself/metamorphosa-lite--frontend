eb5d1f20d51fe2add675a18722f9f435add5a44a
VladyslavHimself <vladyslavhimself@gmail.com>
2021-10-26 13:14:14 -0400

    added response to server

--- components/Auth/Auth.tsx
+++ components/Auth/Auth.tsx
@@ -3,13 +3,26 @@ import { Button } from '../Button/Button';
 import { Input } from '../Input/Input';
 import classes from './Auth.module.scss';
 
+import axios from 'axios';
+
+
 export const Auth = (): JSX.Element => {
 
   const [emailInput, setEmailInput] = useState<string>('');
   const [passwordInput, setPasswordInput] = useState<string>('');
   
-  const onButtonClickHandler = (): void => {
-    // #TODO login 
+  const onButtonClickHandler = async (): Promise<void> => {
+    const LOGIN_API = 'https://www.mcteaparty.fun/api/metamorph/auth/login';
+    console.log('logging...');
+
+    const response = await axios.post(LOGIN_API, {
+      "email": emailInput,
+      "password": passwordInput
+    }).then((data) => {
+      console.log(data);
+    }).catch((e) => {
+      console.log('Cannot access data from server...');
+    });
   };
 
   return (
@@ -19,8 +32,18 @@ export const Auth = (): JSX.Element => {
       </div>
       <hr />
       <div className={classes['auth__inputField']}>
-        <Input type='text' placeholder='E-mail' setData={setEmailInput} />
-        <Input type='password' placeholder='Password' setData={setPasswordInput} />
+        <Input
+         type='text'
+         placeholder='E-mail'
+         setData={setEmailInput}
+        />
+
+        <Input
+         type='password'
+         placeholder='Password'
+         setData={setPasswordInput}
+        />
+
       </div>
 
       <Button value='LogIn' onClickHandler={onButtonClickHandler} />
