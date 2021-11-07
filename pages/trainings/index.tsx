71cde32eed3539fdb98ce368fca30f155319bced
VladyslavHimself <vladyslavhimself@gmail.com>
2021-11-06 23:31:26 -0400

    added new service

--- pages/trainings/index.tsx
+++ pages/trainings/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next';
+import Head from 'next/head';
 import React, { useEffect, useState } from 'react'
 import { TrainingList } from '../../src/components/TrainingList/TrainingList';
 import { Wrapper } from '../../src/containers/Wrapper/Wrapper';
@@ -8,11 +9,19 @@ const Trainings: NextPage = (): JSX.Element => {
   const [isAuthorized, setIsAuthorized] = useState<boolean>(false);
 
   const PlannerTmpl = (
+    
+    <>
+    <Head>
+      <title>Metamorpohosa Lite</title>
+      <meta name="description" content="A lite version of metamorphosa" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
     <Wrapper mode='development'>
       <div className="planner">
         <TrainingList />
       </div>
     </Wrapper>
+    </>
   )
 
 
