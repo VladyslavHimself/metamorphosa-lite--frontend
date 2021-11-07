4e7da2d596239079b39d19272ce458a940662e84
VladyslavHimself <vladyslavhimself@gmail.com>
2021-11-06 23:36:10 -0400

    TODO: excerciseList service

--- src/services/ExcerciseListAPI/ExcerciseListAPI.service.ts
+++ src/services/ExcerciseListAPI/ExcerciseListAPI.service.ts
@@ -1,3 +1,15 @@
 export class ExcerciseListAPI {
-  
+  trainingId: number;
+
+  constructor(trainingId: number) {
+    this.trainingId = trainingId;
+  }
+
+  /**
+   * addExcerciseToTraining
+   */
+  public addExcerciseToTraining() {
+    
+  }
+
 }
