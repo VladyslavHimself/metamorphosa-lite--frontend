9acf26acfa832e05422e6f7624999d649a5e95f5
VladyslavHimself <vladyslavhimself@gmail.com>
2021-11-09 18:47:34 -0500

    Added Date API to make easier routine

---
+++ src/services/DateAPI/DateAPI.service.ts
@@ -0,0 +1,26 @@
+export class DateAPI {
+
+  constructor() {
+
+  }
+
+  /**
+   * Format to readable date.
+   * 
+   * Example: Mon Nov 08 2021
+   */
+  public formatDate(date: string): string {
+    return new Date(date).toDateString();
+  }
+
+  /**
+   * Check is input date similar with present
+   */
+  public isPresentDay(date: string): boolean {
+    if (new Date(date).toDateString() === new Date().toDateString()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
