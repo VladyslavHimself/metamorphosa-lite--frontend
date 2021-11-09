d17440fbf824e942b54cc7daf6e04200db185d1e
VladyslavHimself <vladyslavhimself@gmail.com>
2021-11-09 13:46:03 -0500

    improved layout. Now ExcerciseList do not display time

--- pages/trainings/[id].tsx
+++ pages/trainings/[id].tsx
@@ -1,40 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import classes from '../../styles/training.module.scss';
 import { NextPage } from 'next';
 import { useRouter } from 'next/dist/client/router';
-import React, { useEffect, useState } from 'react';
 import { ExcerciseList } from '../../src/components/ExcerciseList/ExcerciseList';
 import { ExcerciseListAPI } from '../../src/services/ExcerciseListAPI/ExcerciseListAPI.service';
 import { IExcercise } from '../../src/services/ExcerciseListAPI/ExcerciseList.interface';
-import { Wrapper } from '../../src/containers/Wrapper/Wrapper';
 import { Button } from '../../src/components/Button/Button';
 
-import classes from '../../styles/training.module.scss';
+
+import { TrainingListAPI } from '../../src/services/TrainingListAPI/TrainingListAPI.service';
 
 const Training: NextPage = () => {
   const [excercises, setExcercises] = useState<IExcercise[]>();
+  const [trainingDate, setTrainingDate] = useState<any>();
   const router = useRouter();
   const { query } = router;
   
+  // #TODO: With useCallback
+  async function loadExcercises() {
+    const excerciseList = new ExcerciseListAPI();
+    const id: number = +query.id!;
+    const excercisesData = await excerciseList.getExcercisesFromTraining(id);
+    setExcercises(excercisesData);
+    console.log(excercisesData);
+  }
+
+  const convertDateToString = (date: string): string => new Date(date).toDateString();
+
+  async function loadDate() {
+    const tList = new TrainingListAPI();
+      const response = await tList.getTrainingList();
+      response.map((e: {id: number, date: string}) => {
+       if ('' + e.id === query.id) {
+        setTrainingDate(convertDateToString(e.date));
+        
+       }
+      });
+  }
+
   useEffect(() => {
     if (!router.isReady) return;
-    
-    const callback = async () => {
-      const excerciseList = new ExcerciseListAPI();
-      const id: number = +query.id!
-      const excercisesData = await excerciseList.getExcercisesFromTraining(id);
-      setExcercises(excercisesData);
-    }
-    callback();      
-
+         loadExcercises();
+         loadDate();
     return () => {
       console.log('clean');
     }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [router.isReady, query.id]);
 
   return (
       <div className={classes.training}>
-        <Button> Add new excercise</Button>
+        <span className={classes.training__date}>{trainingDate}</span>
+        <Button>Add new excercise</Button>
         <hr />
-        <ExcerciseList excercises={excercises} query={query}/>
+        <ExcerciseList excercises={excercises} />
       </div>
   )
 };
--- src/components/ExcerciseList/ExcerciseList.tsx
+++ src/components/ExcerciseList/ExcerciseList.tsx
@@ -1,56 +1,27 @@
-import { NextPage } from 'next';
-import { useRouter } from 'next/dist/client/router';
-
-import React, { useEffect, useState } from 'react'
-import { IExcercise } from '../../services/ExcerciseListAPI/ExcerciseList.interface';
-import { TrainingListAPI } from '../../services/TrainingListAPI/TrainingListAPI.service';
-import { ExcerciseCard } from '../ExcerciseCard/ExcerciseCard';
+import React from 'react';
 import classes from './ExcerciseList.module.scss';
+import { IExcercise } from '../../services/ExcerciseListAPI/ExcerciseList.interface';
+import { ExcerciseCard } from '../ExcerciseCard/ExcerciseCard';
 
 interface IProps {
   excercises: IExcercise[] | undefined,
-  query: any,
 }
 
-export const ExcerciseList = ({ excercises, query}: IProps) => {
-
-  const router = useRouter();
-  
-  const [currentId, setCurrentId] = useState<any>();
-
-  useEffect(() => {
-    const callback = async () => {
-      const tList = new TrainingListAPI();
-      const response = await tList.getTrainingList();
-      response.map((e: { id: number; date: string; }) => {
-       if (e.id == query.id) {
-        setCurrentId(e.date);
-       }
-      });
-    };
-    callback();
-    
-  }, [query.id]);
-
-  return (
-    <div className={classes['excercise-list']}>
-      <h4 className={classes['excercise-list__date']}>{new Date(currentId).toDateString()}</h4>
-        <div className={classes['excercises-container']}>
-          {
-            excercises && excercises.length ? excercises.map((excercise: IExcercise) => {
-              return (
-                <ExcerciseCard
-                  key={excercise.id}
-                  name={excercise.name}
-                  reps={excercise.reps}
-                  sets={excercise.sets}
-                  weight={excercise.weight}
-                  isCreate={excercise.isCreate}
-                />
-              )
-            }) : <p>You dont have any excercises here :)</p>
-          }
-        </div>
-      </div>
-  )
-};
+export const ExcerciseList = ({ excercises}: IProps) => (
+  <div className={classes['excercise-list']}>
+    {
+      excercises && excercises.length ? excercises.map((excercise: IExcercise) => {
+        return (
+          <ExcerciseCard
+            key={excercise.id}
+            name={excercise.name}
+            reps={excercise.reps}
+            sets={excercise.sets}
+            weight={excercise.weight}
+            isCreate={excercise.isCreate}
+          />
+        )
+      }) : <p>You dont have any excercises here :)</p>
+    }
+  </div>
+);
