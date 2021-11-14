33faafaf187f1336ca7968c3490eecccb7b43d26
VladyslavHimself <vladyslavhimself@gmail.com>
2021-11-13 21:17:01 -0500

    add basic excercise page layout

--- pages/add-excercise/index.tsx
+++ pages/add-excercise/index.tsx
@@ -1,7 +1,10 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/dist/client/router';
 import React, { useEffect, useState } from 'react'
+import { Button } from '../../src/components/Ui/Button/Button';
+import { Input } from '../../src/components/Ui/Input/Input';
 import { Wrapper } from '../../src/containers/Wrapper/Wrapper';
+import classes from '../../styles/AddExcercisePage.module.scss';
 
 interface IQuery {
   id: number,
@@ -12,6 +15,10 @@ const AddExcercisePage: NextPage = (): JSX.Element => {
 
   const [excerciseId, setExcerciseId] = useState<number>();
   const [excerciseName, setExcerciseName] = useState<string>('');
+
+  const [sets, setSets] = useState<string>('');
+  const [reps, setReps] = useState<string>('');
+  const [weight, setWeight] = useState<string>('');
   const router = useRouter();
 
   useEffect(() => {
@@ -20,11 +27,23 @@ const AddExcercisePage: NextPage = (): JSX.Element => {
       id && setExcerciseId(+id);
       name && setExcerciseName('' + name);
     }
-  }, [router.isReady])
+  }, [router.isReady, sets])
+
+  const onAddExcerciseHandler = () => {
+    console.log('add excercise to list');
+  }
 
   return (
     <Wrapper mode={'development'}>
-      <h1>{excerciseId} & {excerciseName}</h1>
+      <div className={classes['excercise-info']}>
+        <div className={classes['excercise-info__header']}>{excerciseName}</div>
+        <div className={classes['excercise-info__container']}>
+          <Input type='text' changeInputState={setSets} placeholder='Sets:' />
+          <Input type='text' changeInputState={setReps} placeholder='Reps:' />
+          <Input type='text' changeInputState={setWeight} placeholder='Weight:' />
+        </div>
+        <Button type='text-based' onClickHandler={onAddExcerciseHandler}>Add excercise</Button>
+      </div>
     </Wrapper>
     
   )
