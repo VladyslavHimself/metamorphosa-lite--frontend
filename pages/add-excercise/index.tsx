ab5c69b9fa1683507305c951191de7415fbd34e9
VladyslavHimself <vladyslavhimself@gmail.com>
2021-11-14 22:07:00 -0500

    clean template

--- pages/add-excercise/index.tsx
+++ pages/add-excercise/index.tsx
@@ -11,38 +11,17 @@ interface IQuery {
   name: string,
 }
 
-const AddExcercisePage: NextPage = (): JSX.Element => {
-
-  const [excerciseId, setExcerciseId] = useState<number>();
-  const [excerciseName, setExcerciseName] = useState<string>('');
-  const [initialId, setInitialId] = useState<number>();
-
-  const [sets, setSets] = useState<string>('');
-  const [reps, setReps] = useState<string>('');
-  const [weight, setWeight] = useState<string>('');
-  const router = useRouter();
-
-  useEffect(() => {
-    if(router.isReady) {
-      const {id, name, trainingId} = router.query;
-      id && setExcerciseId(+id);
-      name && setExcerciseName('' + name);
-      trainingId && setInitialId(+trainingId);
-    }
-  }, [router.isReady, sets])
-
-  const onAddExcerciseHandler = () => {
-    console.log('add excercise to list');
-  }
+const AddExcercisePage: NextPage = (): JSX.Element => { 
+  const onAddExcerciseHandler = () => {};
 
   return (
     <Wrapper mode={'development'}>
       <div className={classes['excercise-info']}>
-        <div className={classes['excercise-info__header']}>{excerciseName}</div>
+        <div className={classes['excercise-info__header']}>{'Pulll'}</div>
         <div className={classes['excercise-info__container']}>
-          <Input type='text' changeInputState={setSets} placeholder='Sets:' />
-          <Input type='text' changeInputState={setReps} placeholder='Reps:' />
-          <Input type='text' changeInputState={setWeight} placeholder='Weight:' />
+          <Input type='text' changeInputState={} placeholder='Sets:' />
+          <Input type='text' changeInputState={} placeholder='Reps:' />
+          <Input type='text' changeInputState={} placeholder='Weight:' />
         </div>
         <Button type='text-based' onClickHandler={onAddExcerciseHandler}>Add excercise</Button>
       </div>
