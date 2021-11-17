d22ce1aea7090727210d29b011298d19a2381fd7
VladyslavHimself <vladyslavhimself@gmail.com>
2021-11-16 20:35:24 -0500

    initialized add-pattern page

---
+++ pages/add-pattern/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+const AddPatternPage: NextPage = () => {
+  return (
+    <h1>Hello world!</h1>
+  )
+};
+
+export default AddPatternPage;
