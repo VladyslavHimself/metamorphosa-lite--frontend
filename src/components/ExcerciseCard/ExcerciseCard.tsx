287260da0490a29c47d207ae949853b8fa47ddf9
VladyslavHimself <vladyslavhimself@gmail.com>
2021-11-16 20:23:21 -0500

    changed line

--- src/components/ExcerciseCard/ExcerciseCard.tsx
+++ src/components/ExcerciseCard/ExcerciseCard.tsx
@@ -6,7 +6,7 @@ export const ExcerciseCard = ({name, reps, sets, weight}: IExcercise) => {
   return (
     <div className={classes['excercise-card']}>
       <div className={classes['excercise-card__name']}>{name}</div>
-      <div className={classes['excercise-card__count']}>{sets}/{reps}</div>
+      <div className={classes['excercise-card__count']}>{sets} | {reps}</div>
       { 
         weight && weight > 0 ? <div className={classes['excercise-card__weight']}>{weight}kg</div> : null 
       }
